bc128b2b4f8370941a403a237f28b05068ef795e
An Nguyen <annguyenxuan1991@gmail.com>
2017-11-25 14:45:48 +0700

    First commit: Fifaonline 4 by automation behave

---
+++ features/FifaOnline4-CommentFunction.feature
@@ -0,0 +1,6 @@
+# Created by Dell at 11/25/2017
+Feature: #Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
---
+++ features/environment.py
@@ -0,0 +1,27 @@
+
+def before_all(context):
+    pass
+
+def before_feature(context, feature):
+    """
+    Placeholder for any before feature hooks needed
+    """
+    pass
+
+
+def before_scenario(context, scenario):
+    pass
+
+
+def init_browser_session(context):
+    pass
+
+def after_scenario(context, scenario):
+    pass
+
+
+def after_all(context):
+    """
+    Placeholder for any before feature hooks needed
+    """
+    pass
